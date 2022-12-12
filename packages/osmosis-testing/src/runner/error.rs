68281423c7a19b002e217a7a248bee11e2ab6436
Sturdy <sturdy@apollo.farm>
2022-12-12 18:18:01 +0100

    feat: Add derived variants to RunnerError

--- packages/osmosis-testing/src/runner/error.rs
+++ packages/osmosis-testing/src/runner/error.rs
@@ -1,14 +1,25 @@
+use cosmrs::rpc::error::Error as TendermintRpcError;
+use cosmrs::tendermint::Error as TendermintError;
+use cosmrs::ErrorReport;
 use cosmwasm_std::StdError;
 use std::str::Utf8Error;
 use thiserror::Error;
+use tonic::transport::Error as TonicError;
+use tonic::Status;
 
-#[derive(Error, Debug, PartialEq)]
+#[derive(Error, Debug)]
 pub enum RunnerError {
     #[error("unable to encode request")]
     EncodeError(#[from] EncodeError),
 
     #[error("unable to decode response")]
     DecodeError(#[from] DecodeError),
+
+    #[error("Can't decode invalid protobuf: {0}")]
+    ProtoDecodeError(#[from] prost::DecodeError),
+
+    #[error("Can't encode invalid protobuf: {0}")]
+    ProtoEncodeError(#[from] prost::EncodeError),
 
     #[error("query error: {}", .msg)]
     QueryError { msg: String },
@@ -18,6 +29,44 @@ pub enum RunnerError {
 
     #[error("{0}")]
     StdError(#[from] StdError),
+
+    #[error("{0}")]
+    Tendermint(#[from] TendermintError),
+
+    #[error("{0}")]
+    TendermintRpc(#[from] TendermintRpcError),
+
+    #[error("{0}")]
+    ErrorReport(#[from] ErrorReport),
+
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("{0}")]
+    TonicStatus(#[from] Status),
+
+    #[error("{0}")]
+    Tonic(#[from] TonicError),
+
+    #[error("{0}")]
+    ParseInt(#[from] std::num::ParseIntError),
+}
+
+/// We can't derive PartialEq because TendermintRpcError doesn't implement it.
+impl PartialEq for RunnerError {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (RunnerError::EncodeError(a), RunnerError::EncodeError(b)) => a == b,
+            (RunnerError::DecodeError(a), RunnerError::DecodeError(b)) => a == b,
+            (RunnerError::QueryError { msg: a }, RunnerError::QueryError { msg: b }) => a == b,
+            (RunnerError::ExecuteError { msg: a }, RunnerError::ExecuteError { msg: b }) => a == b,
+            (RunnerError::StdError(a), RunnerError::StdError(b)) => a.to_string() == b.to_string(),
+            (RunnerError::TendermintRpc(a), RunnerError::TendermintRpc(b)) => a.0 == b.0,
+            (RunnerError::Io(a), RunnerError::Io(b)) => a.to_string() == b.to_string(),
+            (RunnerError::Tonic(a), RunnerError::Tonic(b)) => a.to_string() == b.to_string(),
+            _ => false,
+        }
+    }
 }
 
 #[derive(Error, Debug)]
